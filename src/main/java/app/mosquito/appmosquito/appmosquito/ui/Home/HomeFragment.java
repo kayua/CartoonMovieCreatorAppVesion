c8ad0ea404abc6248fb4e3b9c140861b5ced378c
kayua <kayuaolequesp@gmail.com>
2021-06-07 21:30:03 -0300

    version

--- src/main/java/app/mosquito/appmosquito/appmosquito/ui/Home/HomeFragment.java
+++ src/main/java/app/mosquito/appmosquito/appmosquito/ui/Home/HomeFragment.java
@@ -1,31 +1,89 @@
 package app.mosquito.appmosquito.appmosquito.ui.Home;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import app.mosquito.appmosquito.appmosquito.R;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    MapView mMapView;
+    private GoogleMap googleMap;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+        mMapView = (MapView) root.findViewById(R.id.mapView);
+        mMapView.onCreate(savedInstanceState);
+
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        mMapView.getMapAsync(new OnMapReadyCallback() {
+            @SuppressLint("MissingPermission")
+            @Override
+            public void onMapReady(GoogleMap mMap) {
+                googleMap = mMap;
+
+                // For dropping a marker at a point on the Map
+                LatLng sydney = new LatLng(-29.7895716, -55.7685971);
+                mMap.addMarker(new MarkerOptions()
+                         .position(sydney)
+                         .title("Sydney")
+                         .snippet("size: 2,2,2"));
+
+                googleMap.setMyLocationEnabled(true);
+            }
+        });
+
 
 
 
         return root;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
     }
 }
