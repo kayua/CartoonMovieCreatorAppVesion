a70b6d970a2631e0898f5d151d4d8ee79a47a37d
kayua <kayuaolequesp@gmail.com>
2021-06-08 20:01:03 -0300

    version 1.0.1.1

--- src/main/java/app/mosquito/appmosquito/appmosquito/ui/Maps/MapsFragment.java
+++ src/main/java/app/mosquito/appmosquito/appmosquito/ui/Maps/MapsFragment.java
@@ -1,13 +1,6 @@
 package app.mosquito.appmosquito.appmosquito.ui.Maps;
 
-import android.Manifest;
 import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,17 +8,10 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.CameraPosition;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 import app.mosquito.appmosquito.appmosquito.R;
 
@@ -53,23 +39,11 @@ public class MapsFragment extends Fragment {
             public void onMapReady(GoogleMap mMap) {
                 googleMap = mMap;
 
-                // For dropping a marker at a point on the Map
-                LatLng sydney = new LatLng(-29.7895716, -55.7685971);
-               // mMap.addMarker(new MarkerOptions()
-               //         .position(sydney)
-               //         .title("Sydney")
-               //         .snippet("size: 2,2,2")
-               //         .icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_mosquito)));
-
-                googleMap.setMyLocationEnabled(true);
-
-                
             }
         });
 
         return rootView;
     }
-
 
 
     @Override
