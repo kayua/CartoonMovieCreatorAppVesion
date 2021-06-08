01e144b0c3769befaa4d5883e634c9e5b85dbde8
kayua <kayuaolequesp@gmail.com>
2021-06-08 13:51:20 -0300

    version

--- src/main/java/app/mosquito/appmosquito/appmosquito/ui/Home/HomeFragment.java
+++ src/main/java/app/mosquito/appmosquito/appmosquito/ui/Home/HomeFragment.java
@@ -1,8 +1,10 @@
 package app.mosquito.appmosquito.appmosquito.ui.Home;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,22 +13,36 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.Circle;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import app.mosquito.appmosquito.appmosquito.R;
+import app.mosquito.appmosquito.appmosquito.ui.Maps.GPS_Sistem.GpsTracker;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
     MapView mMapView;
+    GpsTracker gps;
     private GoogleMap googleMap;
+    Context mContext;
+    private Context context;
 
+    @Override
+    public void onAttach(Context context) {
+        this.context = context;
+        super.onAttach(context);
+    }
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
@@ -46,21 +62,48 @@ public class HomeFragment extends Fragment {
             @Override
             public void onMapReady(GoogleMap mMap) {
                 googleMap = mMap;
+
+                gps = new GpsTracker(getActivity().getApplicationContext());
+                double latitude = gps.getLatitude();
+                double longitude = gps.getLongitude();
+
+                LatLng latLng= new LatLng(latitude, longitude);
+                CameraPosition cameraPosition = new CameraPosition.Builder()
+                        .target(latLng)
+                        .zoom(17)
+                        .bearing(0)                // Sets the orientation of the camera to east
+                        .tilt(40)                   // Sets the tilt of the camera to 30 degrees
+                        .build();
+
+                mMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+                Log.i(Double. toString(latitude),Double. toString(longitude));
                 Circle circle = mMap.addCircle(new CircleOptions()
-                        .center(new LatLng(-29.78, -55.77))
-                        .radius(100)
-                        .strokeColor(0xff018733)
+                        .center(new LatLng(latitude ,longitude))
+                        .radius(35)
+                        .strokeColor(0xff018783)
                         .fillColor(Color.TRANSPARENT)
                         .strokeWidth(3)
 
                 );
+                Circle circle1 = mMap.addCircle(new CircleOptions()
+                        .center(new LatLng(latitude ,longitude))
+                        .radius(1)
+                        .strokeColor(0xff018783)
+                        .fillColor(Color.TRANSPARENT)
+                        .strokeWidth(5)
+
+                );
+                Marker mSydney = mMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(latitude ,longitude))
+                        .title("Sydney")
+                        .snippet("Population: 4,627,300")
+                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.location_icon)));
 
 
-                googleMap.setMyLocationEnabled(true);
             }
         });
 
-
+        Context context;
 
 
         return root;
--- src/main/java/app/mosquito/appmosquito/appmosquito/ui/Maps/GPS_Sistem/GpsTracker.java
+++ src/main/java/app/mosquito/appmosquito/appmosquito/ui/Maps/GPS_Sistem/GpsTracker.java
@@ -1,13 +1,11 @@
 package app.mosquito.appmosquito.appmosquito.ui.Maps.GPS_Sistem;
 
-import android.Manifest;
-import android.app.Activity;
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.app.Service;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -16,23 +14,22 @@ import android.os.IBinder;
 import android.provider.Settings;
 import android.util.Log;
 
-import androidx.core.app.ActivityCompat;
-
 public class GpsTracker extends Service implements LocationListener {
+
     private final Context mContext;
 
-    // flag for GPS status
+    // Flag for GPS status
     boolean isGPSEnabled = false;
 
-    // flag for network status
+    // Flag for network status
     boolean isNetworkEnabled = false;
 
-    // flag for GPS status
+    // Flag for GPS status
     boolean canGetLocation = false;
 
-    Location location; // location
-    double latitude; // latitude
-    double longitude; // longitude
+    Location location; // Location
+    double latitude; // Latitude
+    double longitude; // Longitude
 
     // The minimum distance to change Updates in meters
     private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // 10 meters
@@ -40,7 +37,7 @@ public class GpsTracker extends Service implements LocationListener {
     // The minimum time between updates in milliseconds
     private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1; // 1 minute
 
-    // Declaring a Localization Manager
+    // Declaring a Location Manager
     protected LocationManager locationManager;
 
     public GpsTracker(Context context) {
@@ -48,61 +45,50 @@ public class GpsTracker extends Service implements LocationListener {
         getLocation();
     }
 
+    @SuppressLint("MissingPermission")
     public Location getLocation() {
         try {
-            locationManager = (LocationManager) mContext.getSystemService(LOCATION_SERVICE);
+            locationManager = (LocationManager) mContext
+                    .getSystemService(LOCATION_SERVICE);
 
-            // getting GPS status
-            isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            // Getting GPS status
+            isGPSEnabled = locationManager
+                    .isProviderEnabled(LocationManager.GPS_PROVIDER);
 
-            // getting network status
+            // Getting network status
             isNetworkEnabled = locationManager
                     .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
 
             if (!isGPSEnabled && !isNetworkEnabled) {
-                // no network provider is enabled
+                // No network provider is enabled
             } else {
                 this.canGetLocation = true;
-                // First get location from Network Provider
                 if (isNetworkEnabled) {
-                    //check the network permission
-                    if (ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                        ActivityCompat.requestPermissions((Activity) mContext, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 101);
-                    }
                     locationManager.requestLocationUpdates(
                             LocationManager.NETWORK_PROVIDER,
                             MIN_TIME_BW_UPDATES,
                             MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
-
                     Log.d("Network", "Network");
                     if (locationManager != null) {
                         location = locationManager
                                 .getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-
                         if (location != null) {
                             latitude = location.getLatitude();
                             longitude = location.getLongitude();
                         }
                     }
                 }
-
-                // if GPS Enabled get lat/long using GPS Services
+                // If GPS enabled, get latitude/longitude using GPS Services
                 if (isGPSEnabled) {
                     if (location == null) {
-                        //check the network permission
-                        if (ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                            ActivityCompat.requestPermissions((Activity) mContext, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 101);
-                        }
                         locationManager.requestLocationUpdates(
                                 LocationManager.GPS_PROVIDER,
                                 MIN_TIME_BW_UPDATES,
                                 MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
-
                         Log.d("GPS Enabled", "GPS Enabled");
                         if (locationManager != null) {
                             location = locationManager
                                     .getLastKnownLocation(LocationManager.GPS_PROVIDER);
-
                             if (location != null) {
                                 latitude = location.getLatitude();
                                 longitude = location.getLongitude();
@@ -111,29 +97,29 @@ public class GpsTracker extends Service implements LocationListener {
                     }
                 }
             }
-
-        } catch (Exception e) {
+        }
+        catch (Exception e) {
             e.printStackTrace();
         }
 
         return location;
     }
 
+
     /**
      * Stop using GPS listener
-     * Calling this function will stop using GPS in your app
+     * Calling this function will stop using GPS in your app.
      * */
-
     public void stopUsingGPS(){
         if(locationManager != null){
             locationManager.removeUpdates(GpsTracker.this);
         }
     }
 
+
     /**
      * Function to get latitude
      * */
-
     public double getLatitude(){
         if(location != null){
             latitude = location.getLatitude();
@@ -143,10 +129,10 @@ public class GpsTracker extends Service implements LocationListener {
         return latitude;
     }
 
+
     /**
      * Function to get longitude
      * */
-
     public double getLongitude(){
         if(location != null){
             longitude = location.getLongitude();
@@ -157,19 +143,18 @@ public class GpsTracker extends Service implements LocationListener {
     }
 
     /**
-     * Function to check GPS/wifi enabled
+     * Function to check GPS/Wi-Fi enabled
      * @return boolean
      * */
-
     public boolean canGetLocation() {
         return this.canGetLocation;
     }
 
-    /**
-     * Function to show settings alert dialog
-     * On pressing Settings button will lauch Settings Options
-     * */
 
+    /**
+     * Function to show settings alert dialog.
+     * On pressing the Settings button it will launch Settings Options.
+     * */
     public void showSettingsAlert(){
         AlertDialog.Builder alertDialog = new AlertDialog.Builder(mContext);
 
@@ -179,38 +164,45 @@ public class GpsTracker extends Service implements LocationListener {
         // Setting Dialog Message
         alertDialog.setMessage("GPS is not enabled. Do you want to go to settings menu?");
 
-        // On pressing Settings button
+        // On pressing the Settings button.
         alertDialog.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog,int which) {
+            public void onClick(DialogInterface dialog, int which) {
                 Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
                 mContext.startActivity(intent);
             }
         });
 
-        // on pressing cancel button
+        // On pressing the cancel button
         alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int which) {
                 dialog.cancel();
             }
         });
+
+        // Showing Alert Message
         alertDialog.show();
     }
+
 
     @Override
     public void onLocationChanged(Location location) {
     }
 
+
     @Override
     public void onProviderDisabled(String provider) {
     }
+
 
     @Override
     public void onProviderEnabled(String provider) {
     }
 
+
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {
     }
+
 
     @Override
     public IBinder onBind(Intent arg0) {
