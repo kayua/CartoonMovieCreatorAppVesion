f08405c9497ebcf603009e16a5b9ab5087832593
kayua <kayuaolequesp@gmail.com>
2021-06-08 16:53:07 -0300

    version

--- src/main/java/app/mosquito/appmosquito/appmosquito/ui/Maps/Gradient_map.java
+++ src/main/java/app/mosquito/appmosquito/appmosquito/ui/Maps/Gradient_map.java
@@ -2,13 +2,13 @@ package app.mosquito.appmosquito.appmosquito.ui.Maps;
 
 import android.util.Log;
 
-import androidx.collection.CircularArray;
-import androidx.collection.SimpleArrayMap;
-
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Gradient_map {
 
@@ -17,16 +17,23 @@ public class Gradient_map {
         ParseUser.logInInBackground("Admin", "admin", (user, e) -> {
             ParseQuery<ParseObject> query = ParseQuery.getQuery("Detections");
 
-            List<String>
-            ParseObject a = null;
-            try {
-                a = query.getFirst();
-            } catch (ParseException parseException) {
-                parseException.printStackTrace();
-            }
+            List<String> latitude = new ArrayList<>();
+            List<String> longitude = new ArrayList<>();
+            List<String> datetime = new ArrayList<>();
 
-            String b = (String) a.get("latitude");
-                Log.i("Latitude********************************************", b);
+            try {
+                List<ParseObject> results = query.find();
+                for (ParseObject result : results) {
+                    ParseObject a = query.get(result.getObjectId());
+                    latitude.add((String) a.get("latitude"));
+                    longitude.add((String) a.get("longitude"));
+                    longitude.add((String) a.get("DataTime"));
+
+
+                }
+            } catch (ParseException ei) {
+                e.printStackTrace();
+            }
 
 
         });
