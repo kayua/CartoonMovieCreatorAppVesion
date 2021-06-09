2b0a199ea0e7280bed8490f49dc96cb7c0c92381
kayua <kayuaolequesp@gmail.com>
2021-06-09 16:38:17 -0300

    version 1.0.1.1

--- src/main/java/app/mosquito/appmosquito/appmosquito/ui/Statistics/StatisticsFragment.java
+++ src/main/java/app/mosquito/appmosquito/appmosquito/ui/Statistics/StatisticsFragment.java
@@ -16,6 +16,7 @@ import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -35,23 +36,24 @@ public class StatisticsFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_statistics, container, false);
 
         BarChart bchart = (BarChart) root.findViewById(R.id.barchart);
-
+        bchart.setBorderColor(1);
         ArrayList<BarEntry> yVals1 = new ArrayList<BarEntry>();
 
-        for (int i = (int) 0; i < 10 + 1; i++) {
+        for (int i = (int) 0; i < 12 + 1; i++) {
             float val = (float) (Math.random());
             yVals1.add(new BarEntry(i, val));
         }
 
+
         BarDataSet set1;
 
         set1 = new BarDataSet(yVals1, "The year 2017");
-        set1.addColor(1);
-        set1.addColor(200);
+
+
 
         ArrayList<IBarDataSet> dataSets = new ArrayList<IBarDataSet>();
         dataSets.add(set1);
-
+        set1.setColors(ColorTemplate.LIBERTY_COLORS);
         BarData data = new BarData(dataSets);
 
         data.setValueTextSize(10f);
@@ -59,6 +61,7 @@ public class StatisticsFragment extends Fragment {
 
         bchart.setTouchEnabled(false);
         bchart.setData(data);
+
         galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
