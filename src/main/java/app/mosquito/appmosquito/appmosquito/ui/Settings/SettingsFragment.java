c66d698426c63182cf0e3385a0d9b68ae50b600f
kayua <kayuaolequesp@gmail.com>
2021-06-12 03:14:11 -0300

    version 1.0.1.1

--- src/main/java/app/mosquito/appmosquito/appmosquito/ui/Settings/SettingsFragment.java
+++ src/main/java/app/mosquito/appmosquito/appmosquito/ui/Settings/SettingsFragment.java
@@ -32,106 +32,99 @@ public class SettingsFragment extends Fragment {
         Switch lowPower = (Switch) root.findViewById(R.id.lowPowerSwitch);
         String switchLowPower = settings.getString("lowPower", "");
         if(switchLowPower.equals("on")){
-            lowPower.setChecked(true); }else{
-            lowPower.setChecked(false); }
+
+            lowPower.setChecked(true); }else{ lowPower.setChecked(false); }
 
         Switch geoPrecision = (Switch) root.findViewById(R.id.geoPrecisionSwitch);
         String switchGeoPrecision = settings.getString("geoPrecision", "");
         if(switchGeoPrecision.equals("on")){
-            geoPrecision.setChecked(true); }else{
-            geoPrecision.setChecked(false); }
+
+            geoPrecision.setChecked(true); }else{ geoPrecision.setChecked(false); }
 
         Switch autoStart = (Switch) root.findViewById(R.id.autoStartSwitch);
         String switchAutoStart = settings.getString("autoStart", "");
         if(switchAutoStart.equals("on")){
-            autoStart.setChecked(true); }else{
-            autoStart.setChecked(false); }
+
+            autoStart.setChecked(true); }else{ autoStart.setChecked(false); }
 
         Switch emailNotification = (Switch) root.findViewById(R.id.emailNotificationSwitch);
         String switchEmailNotification = settings.getString("emailNotification", "");
         if(switchEmailNotification.equals("on")){
-            emailNotification.setChecked(true); }else{
-            emailNotification.setChecked(false); }
+
+            emailNotification.setChecked(true); }else{ emailNotification.setChecked(false); }
 
         Switch realTimeNotification = (Switch) root.findViewById(R.id.realTimeNotificationSwitch);
         String switchRealTimeNotification = settings.getString("realTimeNotification", "");
         if(switchRealTimeNotification.equals("on")){
-            realTimeNotification.setChecked(true); }else{
-            realTimeNotification.setChecked(false); }
+
+            realTimeNotification.setChecked(true); }else{ realTimeNotification.setChecked(false); }
 
         Switch infectionNotification = (Switch) root.findViewById(R.id.infectNotificationSwitch);
         String switchInfectionNotification = settings.getString("infectionNotification", "");
         if(switchInfectionNotification.equals("on")){
-            infectionNotification.setChecked(true); }else{
-            infectionNotification.setChecked(false); }
+
+            infectionNotification.setChecked(true);}else{infectionNotification.setChecked(false); }
 
         Switch soundNotification = (Switch) root.findViewById(R.id.soundNotificationSwitch);
         String switchSoundNotification = settings.getString("soundNotification", "");
         if(switchSoundNotification.equals("on")){
-            soundNotification.setChecked(true); }else{
-            soundNotification.setChecked(false); }
+
+            soundNotification.setChecked(true); }else{ soundNotification.setChecked(false); }
 
         Switch advancedFilters = (Switch) root.findViewById(R.id.advancedNotificationSwitch);
         String switchAdvancedFilters = settings.getString("advancedFilters", "");
         if(switchAdvancedFilters.equals("on")){
-            advancedFilters.setChecked(true); }else{
-            advancedFilters.setChecked(false); }
+
+            advancedFilters.setChecked(true); }else{ advancedFilters.setChecked(false); }
 
         Switch rotation = (Switch) root.findViewById(R.id.rotationSwitch);
         String switchRotation = settings.getString("rotationSwitch", "");
         if(switchRotation.equals("on")){
-            rotation.setChecked(true); }else{
-            rotation.setChecked(false); }
+
+            rotation.setChecked(true); }else{ rotation.setChecked(false); }
 
         Switch humidity = (Switch) root.findViewById(R.id.sensorHumiditySwitch);
         String switchHumidity = settings.getString("humiditySensor", "");
         if(switchHumidity.equals("on")){
-            humidity .setChecked(true); }else{
-            humidity .setChecked(false); }
+
+            humidity.setChecked(true); }else{ humidity.setChecked(false); }
 
         Switch pressure = (Switch) root.findViewById(R.id.sensorPressureSwitch);
         String switchPressure= settings.getString("pressureSensor", "");
         if(switchPressure.equals("on")){
-            pressure.setChecked(true); }else{
-            pressure.setChecked(false); }
+
+            pressure.setChecked(true); }else{ pressure.setChecked(false); }
 
         Switch luminosity = (Switch) root.findViewById(R.id.sensorLuminositySwitch);
         String switchLuminosity= settings.getString("luminositySensor", "");
         if(switchLuminosity.equals("on")){
-            luminosity.setChecked(true); }else{
-            luminosity.setChecked(false); }
+
+            luminosity.setChecked(true); }else{ luminosity.setChecked(false); }
 
         Switch temperature = (Switch) root.findViewById(R.id.sensorTemperatureSwitch);
         String switchTemperature = settings.getString("temperatureSensor", "");
         if(switchTemperature.equals("on")){
-            temperature.setChecked(true); }else{
-            temperature.setChecked(false); }
+
+            temperature.setChecked(true); }else{ temperature.setChecked(false); }
 
         Switch dayActivity = (Switch) root.findViewById(R.id.dayActivitySwitch);
         String switchDayActivity  = settings.getString("dayActivity", "");
         if(switchDayActivity.equals("on")){
-            dayActivity.setChecked(true); }else{
-            dayActivity.setChecked(false); }
+
+            dayActivity.setChecked(true); }else{ dayActivity.setChecked(false); }
 
         Switch nightActivity = (Switch) root.findViewById(R.id.nightActivitySwitch);
         String switchNightActivity  = settings.getString("nightActivity", "");
         if(switchDayActivity.equals("on")){
-            nightActivity.setChecked(true); }else{
-            nightActivity.setChecked(false); }
+
+            nightActivity.setChecked(true); }else{ nightActivity.setChecked(false); }
 
         lowPower.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                if(lowPower.isChecked()){
-
-                    editor.putString("lowPower", "on");
-
-                }else{
-
-                    editor.putString("lowPower", "off");
-
-                }
+                if(lowPower.isChecked()){ editor.putString("lowPower", "on");
+                }else{ editor.putString("lowPower", "off"); }
                 editor.commit();
 
             }});
@@ -140,15 +133,8 @@ public class SettingsFragment extends Fragment {
             @Override
             public void onClick(View view) {
 
-                if (geoPrecision.isChecked()){
-
-                    editor.putString("geoPrecision", "on");
-
-                }else{
-
-                    editor.putString("geoPrecision", "off");
-
-                }
+                if (geoPrecision.isChecked()){ editor.putString("geoPrecision", "on");
+                }else{ editor.putString("geoPrecision", "off"); }
                 editor.commit();
 
             }});
@@ -157,15 +143,8 @@ public class SettingsFragment extends Fragment {
             @Override
             public void onClick(View view) {
 
-                if(autoStart.isChecked()){
-
-                    editor.putString("autoStart", "on");
-
-                }else{
-
-                    editor.putString("autoStart", "off");
-
-                }
+                if(autoStart.isChecked()){ editor.putString("autoStart", "on");
+                }else{ editor.putString("autoStart", "off"); }
                 editor.commit();
             }});
 
@@ -173,207 +152,98 @@ public class SettingsFragment extends Fragment {
             @Override
             public void onClick(View view) {
 
-                if(emailNotification.isChecked()){
-
-                editor.putString("emailNotification", "on");
-
-                }else{
-
-                editor.putString("emailNotification", "off");
-                }
-                editor.commit();
-        }
-
-        });
+                if(emailNotification.isChecked()){ editor.putString("emailNotification", "on");
+                }else{ editor.putString("emailNotification", "off"); }
+                editor.commit(); }});
 
         realTimeNotification.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                if(realTimeNotification.isChecked()){
-
-                    editor.putString("realTimeNotification", "on");
-
-                }else{
-
-                    editor.putString("realTimeNotification", "off");}
-                editor.commit();
-            }
-
-            });
+                if(realTimeNotification.isChecked()){ editor.putString("realTimeNotification", "on");
+                }else{ editor.putString("realTimeNotification", "off");}
+                editor.commit(); }});
 
         infectionNotification.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                if(infectionNotification.isChecked()){
-
-                    editor.putString("infectionNotification", "on");
-
-                }else{
-
-                    editor.putString("infectionNotification", "off");
-
-                }
-                editor.commit();
-            }});
+                if(infectionNotification.isChecked()){ editor.putString("infectionNotification", "on");
+                }else{ editor.putString("infectionNotification", "off"); }
+                editor.commit(); }});
 
         soundNotification.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                if(soundNotification.isChecked()){
-
-                    editor.putString("soundNotification", "on");
-
-            }else{
-
-                editor.putString("soundNotification", "off");
-                }
+                if(soundNotification.isChecked()){ editor.putString("soundNotification", "on");
+                }else{ editor.putString("soundNotification", "off"); }
                 editor.commit();
-            }});
+                }});
 
         advancedFilters.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-
-                if(advancedFilters.isChecked()){
-
-                    editor.putString("advancedFilters", "on");
-
-
-                }else{
-
-                    editor.putString("advancedFilters", "off");
-
-
-                }
-                editor.commit();
-
-            }});
+                if(advancedFilters.isChecked()){ editor.putString("advancedFilters", "on");
+                }else{ editor.putString("advancedFilters", "off"); }
+                editor.commit(); }});
 
         rotation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                if(rotation.isChecked()){
-
-                    editor.putString("rotationSwitch", "on");
-
-                }else{
-
-
-            editor.putString("rotationSwitch", "off");}
-                editor.commit();
-            }});
+                if(rotation.isChecked()){ editor.putString("rotationSwitch", "on");
+                }else{ editor.putString("rotationSwitch", "off");}
+                editor.commit(); }});
 
         humidity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                if(humidity.isChecked()){
-
-
-
-                    editor.putString("humiditySensor", "on");
-
-
-                }else{
-
-
-                editor.putString("humiditySensor", "off");}
-
-                editor.commit();
-            }
-                });
+                if(humidity.isChecked()){ editor.putString("humiditySensor", "on");
+                }else{ editor.putString("humiditySensor", "off");}
+                editor.commit(); }});
 
         pressure.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                if(pressure.isChecked()){
-
-                    editor.putString("pressureSensor", "on");
-
-                }else{
-
-                    editor.putString("pressureSensor", "off");
-
-                }
-                editor.commit();
-            }});
+                if(pressure.isChecked()){ editor.putString("pressureSensor", "on");
+                }else{ editor.putString("pressureSensor", "off"); }
+                editor.commit(); }});
 
         luminosity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-
-                if(luminosity.isChecked()){
-
-                    editor.putString("luminositySensor", "on");
-                }else{
-
-
-                    editor.putString("luminositySensor", "off");
-                }
-
+                if(luminosity.isChecked()){ editor.putString("luminositySensor", "on");
+                }else{ editor.putString("luminositySensor", "off"); }
                 editor.commit();}});
 
         dayActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                if(dayActivity.isChecked()){
-
-                    editor.putString("dayActivity", "on");
-
-                }else{
-
-                    editor.putString("dayActivity", "off");
-
-                }
-                editor.commit();
-            }
-        });
+                if(dayActivity.isChecked()){ editor.putString("dayActivity", "on");
+                }else{ editor.putString("dayActivity", "off"); }
+                editor.commit(); }});
 
         nightActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                if(nightActivity.isChecked()){
-
-                    editor.putString("nightActivity", "on");
-
-                }else{
-
-                    editor.putString("nightActivity", "off");
-
-                }
-                editor.commit();
-            }
-        });
+                if(nightActivity.isChecked()){ editor.putString("nightActivity", "on");
+                }else{ editor.putString("nightActivity", "off"); }
+                editor.commit(); }});
 
         temperature.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                if(temperature.isChecked()){
-
-
-                    editor.putString("temperatureSensor", "on");
-
-
-                }else{
-
-                    editor.putString("temperatureSensor", "off");
-
-
-                }
-
-                editor.commit();
-
-            }});
+                if(temperature.isChecked()){ editor.putString("temperatureSensor", "on");
+                }else{ editor.putString("temperatureSensor", "off"); }
+                editor.commit(); }});
 
         settingsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
