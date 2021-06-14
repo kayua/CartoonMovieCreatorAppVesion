76039f8c65b85da2dc0dab2dadbf8cc7917acd59
kayua <kayuaolequesp@gmail.com>
2021-06-14 19:19:10 -0300

    version 1.0.1.1

--- src/main/java/app/mosquito/appmosquito/appmosquito/Authentication/ActivityAcessAccount.java
+++ src/main/java/app/mosquito/appmosquito/appmosquito/Authentication/ActivityAcessAccount.java
@@ -1,13 +1,11 @@
 package app.mosquito.appmosquito.appmosquito.Authentication;
 
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.Switch;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -18,8 +16,6 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.parse.Parse;
-import com.parse.ParseUser;
 
 import app.mosquito.appmosquito.appmosquito.R;
 
@@ -27,11 +23,11 @@ import app.mosquito.appmosquito.appmosquito.R;
 public class ActivityAcessAccount extends AppCompatActivity {
 
     public static final String PREFS_NAME = "PersonalDatabase";
-    private EditText username, password, email;
+    private EditText username, password;
     private CheckBox license;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
-    private Switch active_email_notification, active_realtime_notification, active_infestations_detect;
+
 
     public ActivityAcessAccount() {
     }
@@ -40,13 +36,8 @@ public class ActivityAcessAccount extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        Parse.initialize(new Parse.Configuration.Builder(this)
-                .applicationId("zaCP2VZI9fujPU1PpQMzoXYVK75Q5KvhTOmJMoM7")
-                .clientKey("FWqAGbF91QUtnDtv3h7XpKh6KQBiQCBmiDW6aG0d")
-                .server("https://parseapi.back4app.com")
-                .build()
-        );
-        setContentView(R.layout.auth_register);
+
+        setContentView(R.layout.auth_acess);
         mAuth = FirebaseAuth.getInstance();
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
@@ -119,63 +110,7 @@ public class ActivityAcessAccount extends AppCompatActivity {
 
     public void createUser(View view) throws InterruptedException {
 
-        username = (EditText) findViewById(R.id.username_id);
-        password = (EditText) findViewById(R.id.password_id);
-        email = (EditText) findViewById(R.id.email_id);
 
-        active_email_notification = (Switch) findViewById(R.id.switch3);
-        active_realtime_notification = (Switch) findViewById(R.id.switch10);
-        active_infestations_detect = (Switch) findViewById(R.id.switch2);
-        license = (CheckBox) findViewById(R.id.checkBox2);
-
-        if (username.getText().toString().isEmpty() || password.getText().toString().isEmpty()){
-
-
-            Toast.makeText(this, "Preencha os campos obrigatórios", Toast.LENGTH_SHORT).show();
-
-        }else{
-
-            ParseUser user = new ParseUser();
-            user.setUsername(username.getText().toString());
-
-            user.setPassword(password.getText().toString());
-            user.setEmail(email.getText().toString());
-            user.put("phoneNumber", "99999999");
-
-            user.put("email_notification", active_email_notification.isChecked());
-            user.put("realtime_notification", active_realtime_notification.isChecked());
-            user.put("infection_notification", active_infestations_detect.isChecked());
-
-            if (license.isChecked()) {
-
-                user.signUpInBackground(e -> {
-
-                    if (e == null) {
-
-                        Toast.makeText(this, "Cadastro efetuado com sucesso", Toast.LENGTH_SHORT).show();
-
-                        store_login();
-                        Intent i = new Intent(ActivityAcessAccount.this, ActivityEmailChecking.class);
-                        finish();
-                        startActivity(i);
-
-                    } else {
-
-                        Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
-
-                    }
-
-                });
-
-            } else {
-
-                Toast.makeText(this, "É necessário aceitar os termos de uso.", Toast.LENGTH_LONG).show();
-
-            }
-
-            user.getCurrentUser().logOut();
-
-        }
 
     }
 
