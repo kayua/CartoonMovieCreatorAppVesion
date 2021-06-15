6b4e065a72c3ec23d9f2dc00609b1f19806ea96a
kayua <kayuaolequesp@gmail.com>
2021-06-15 16:48:31 -0300

    version 1.0.1.1

--- src/main/java/app/mosquito/appmosquito/appmosquito/Authentication/ActivityEmailChecking.java
+++ src/main/java/app/mosquito/appmosquito/appmosquito/Authentication/ActivityEmailChecking.java
@@ -1,77 +1,49 @@
 package app.mosquito.appmosquito.appmosquito.Authentication;
 
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Switch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.parse.Parse;
-import com.parse.ParseUser;
+import com.google.firebase.auth.FirebaseAuth;
 
-import app.mosquito.appmosquito.appmosquito.ActivityUserInterface;
 import app.mosquito.appmosquito.appmosquito.R;
 
 public class ActivityEmailChecking extends AppCompatActivity {
 
-    String username_registered;
-    String password_registered;
     public static final String PREFS_NAME = "PersonalDatabase";
+    private EditText username, password, email;
+    private CheckBox license;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private Switch active_email_notification, active_realtime_notification, active_infestations_detect;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        Parse.initialize(new Parse.Configuration.Builder(this)
-                .applicationId("zaCP2VZI9fujPU1PpQMzoXYVK75Q5KvhTOmJMoM7")
-                .clientKey("FWqAGbF91QUtnDtv3h7XpKh6KQBiQCBmiDW6aG0d")
-                .server("https://parseapi.back4app.com")
-                .build()
-        );
+
         setContentView(R.layout.auth_email);
+
     }
-
-    public void init_section_user(View view){
-
-        recovery_login();
-        Toast.makeText(this, password_registered, Toast.LENGTH_SHORT).show();
-
-        ParseUser.logInInBackground(username_registered, password_registered, (user, e) -> {
-            if (user != null) {
-
-                boolean emailVerified = user.getBoolean("emailVerified");
-
-                if (emailVerified) {
-
-                    open_mosquito_app(view);
-
-                } else {
-                    Toast.makeText(this, "Seu email não foi verificado", Toast.LENGTH_SHORT).show();
-                }
-            } else {
-
-                Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-        });
-
+    @Override
+    public void onStart() {
+        super.onStart();
 
     }
 
-    public void open_mosquito_app(View view){
-
-        Intent i = new Intent(ActivityEmailChecking.this, ActivityUserInterface.class);
-        finish();
-        startActivity(i);
-
+    @Override
+    public void onStop() {
+        super.onStop();
     }
 
-    private void recovery_login() {
 
-        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
-        username_registered = settings.getString("username", "");
-        password_registered = settings.getString("password", "");
-
-        }
+    public void createUser(View view) throws InterruptedException {
 
 
     }
+}
