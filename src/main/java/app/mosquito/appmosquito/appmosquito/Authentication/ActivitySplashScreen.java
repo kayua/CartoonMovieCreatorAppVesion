3c0def7e8834b1738dc0f0fdb1c386a3e411d5d3
kayua <kayuaolequesp@gmail.com>
2021-06-15 17:28:01 -0300

    version 1.0.1.1

---
+++ src/main/java/app/mosquito/appmosquito/appmosquito/Authentication/ActivitySplashScreen.java
@@ -0,0 +1,77 @@
+package app.mosquito.appmosquito.appmosquito.Authentication;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import app.mosquito.appmosquito.appmosquito.ActivityUserInterface;
+import app.mosquito.appmosquito.appmosquito.R;
+
+public class ActivitySplashScreen extends AppCompatActivity {
+
+    String usernameRegistered;
+    String passwordRegistered;
+    public static final String PREFS_NAME = "PersonalDatabase";
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash_screen);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+            }
+        };
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        usernameRegistered = settings.getString("username", "");
+        passwordRegistered = settings.getString("password", "");
+        mAuth.signInWithEmailAndPassword(usernameRegistered ,
+                passwordRegistered).addOnCompleteListener(this,
+                new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (!task.isSuccessful()) {
+                    screen_start();
+                }else{
+                    screen_user();
+                }
+            }
+        });
+
+    }
+
+
+    private void screen_user(){
+
+        Intent i = new Intent(ActivitySplashScreen.this, ActivityUserInterface.class);
+        finish();
+        startActivity(i);
+    }
+
+    private void screen_start(){
+
+
+
+        Intent i = new Intent(ActivitySplashScreen.this, ActivityStart.class);
+        finish();
+        startActivity(i);
+    }
+
+}
