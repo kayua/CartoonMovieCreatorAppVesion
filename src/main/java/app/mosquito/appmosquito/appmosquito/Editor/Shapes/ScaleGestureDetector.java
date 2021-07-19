bb645a164c6bed78daa666532431989b3efc3068
kayua <kayuaolequesp@gmail.com>
2021-07-18 22:10:39 -0300

    version 1.0.1.1

--- src/main/java/app/mosquito/appmosquito/appmosquito/Editor/Shapes/ScaleGestureDetector.java
+++ src/main/java/app/mosquito/appmosquito/appmosquito/Editor/Shapes/ScaleGestureDetector.java
@@ -72,197 +72,238 @@ class ScaleGestureDetector {
         if (action == MotionEvent.ACTION_DOWN) { reset(); }
 
         boolean handled = true;
+
         if (mInvalidGesture) {
+
             handled = false;
+
         } else if (!mGestureInProgress) {
+
             switch (action) {
+
                 case MotionEvent.ACTION_DOWN: {
+
                     mActiveId0 = event.getPointerId(0);
                     mActive0MostRecent = true;
+
                 }
+
                 break;
 
                 case MotionEvent.ACTION_UP:
+
                     reset();
                     break;
 
                 case MotionEvent.ACTION_POINTER_DOWN: {
-                    // We have a new multi-finger gesture
+
                     if (mPrevEvent != null) mPrevEvent.recycle();
+
                     mPrevEvent = MotionEvent.obtain(event);
                     mTimeDelta = 0;
-
                     int index1 = event.getActionIndex();
                     int index0 = event.findPointerIndex(mActiveId0);
                     mActiveId1 = event.getPointerId(index1);
+
                     if (index0 < 0 || index0 == index1) {
 
                         index0 = findNewActiveIndex(event, mActiveId1, -1);
                         mActiveId0 = event.getPointerId(index0);
+
                     }
+
                     mActive0MostRecent = false;
-
                     setContext(view, event);
-
                     mGestureInProgress = mListener.onScaleBegin(view, this);
                     break;
+
                 }
+
             }
+
         } else {
 
             switch (action) {
+
                 case MotionEvent.ACTION_POINTER_DOWN: {
 
                     mListener.onScaleEnd(view, this);
                     final int oldActive0 = mActiveId0;
                     final int oldActive1 = mActiveId1;
                     reset();
-
                     mPrevEvent = MotionEvent.obtain(event);
                     mActiveId0 = mActive0MostRecent ? oldActive0 : oldActive1;
                     mActiveId1 = event.getPointerId(event.getActionIndex());
                     mActive0MostRecent = false;
-
                     int index0 = event.findPointerIndex(mActiveId0);
+
                     if (index0 < 0 || mActiveId0 == mActiveId1) {
-                        // Probably someone sending us a broken event stream.
+
                         index0 = findNewActiveIndex(event, mActiveId1, -1);
                         mActiveId0 = event.getPointerId(index0);
+
                     }
 
                     setContext(view, event);
-
                     mGestureInProgress = mListener.onScaleBegin(view, this);
+
                 }
+
                 break;
 
                 case MotionEvent.ACTION_POINTER_UP: {
+
                     final int pointerCount = event.getPointerCount();
                     final int actionIndex = event.getActionIndex();
                     final int actionId = event.getPointerId(actionIndex);
-
                     boolean gestureEnded = false;
+
                     if (pointerCount > 2) {
+
                         if (actionId == mActiveId0) {
+
                             final int newIndex = findNewActiveIndex(event, mActiveId1, actionIndex);
+
                             if (newIndex >= 0) {
+
                                 mListener.onScaleEnd(view, this);
                                 mActiveId0 = event.getPointerId(newIndex);
                                 mActive0MostRecent = true;
                                 mPrevEvent = MotionEvent.obtain(event);
                                 setContext(view, event);
                                 mGestureInProgress = mListener.onScaleBegin(view, this);
+
                             } else {
+
                                 gestureEnded = true;
+
                             }
+
                         } else if (actionId == mActiveId1) {
+
                             final int newIndex = findNewActiveIndex(event, mActiveId0, actionIndex);
+
                             if (newIndex >= 0) {
+
                                 mListener.onScaleEnd(view, this);
                                 mActiveId1 = event.getPointerId(newIndex);
                                 mActive0MostRecent = false;
                                 mPrevEvent = MotionEvent.obtain(event);
                                 setContext(view, event);
                                 mGestureInProgress = mListener.onScaleBegin(view, this);
+
                             } else {
+
                                 gestureEnded = true;
+
                             }
+
                         }
+
                         mPrevEvent.recycle();
                         mPrevEvent = MotionEvent.obtain(event);
                         setContext(view, event);
+
                     } else {
+
                         gestureEnded = true;
+
                     }
 
                     if (gestureEnded) {
-                        // Gesture ended
-                        setContext(view, event);
 
-                        // Set focus point to the remaining finger
+                        setContext(view, event);
                         final int activeId = actionId == mActiveId0 ? mActiveId1 : mActiveId0;
                         final int index = event.findPointerIndex(activeId);
                         mFocusX = event.getX(index);
                         mFocusY = event.getY(index);
-
                         mListener.onScaleEnd(view, this);
                         reset();
                         mActiveId0 = activeId;
                         mActive0MostRecent = true;
+
                     }
+
                 }
+
                 break;
 
                 case MotionEvent.ACTION_CANCEL:
+
                     mListener.onScaleEnd(view, this);
                     reset();
                     break;
 
                 case MotionEvent.ACTION_UP:
+
                     reset();
                     break;
 
                 case MotionEvent.ACTION_MOVE: {
+
                     setContext(view, event);
 
-                    // Only accept the event if our relative pressure is within
-                    // a certain limit - this can help filter shaky data as a
-                    // finger is lifted.
                     if (mCurrPressure / mPrevPressure > PRESSURE_THRESHOLD) {
+
                         final boolean updatePrevious = mListener.onScale(view, this);
 
                         if (updatePrevious) {
+
                             mPrevEvent.recycle();
                             mPrevEvent = MotionEvent.obtain(event);
+
                         }
+
                     }
+
                 }
+
                 break;
+
             }
+
         }
 
         return handled;
     }
 
     private int findNewActiveIndex(MotionEvent ev, int otherActiveId, int removedPointerIndex) {
-        final int pointerCount = ev.getPointerCount();
 
-        // It's ok if this isn't found and returns -1, it simply won't match.
+        final int pointerCount = ev.getPointerCount();
         final int otherActiveIndex = ev.findPointerIndex(otherActiveId);
 
-        // Pick a new id and update tracking state.
         for (int i = 0; i < pointerCount; i++) {
-            if (i != removedPointerIndex && i != otherActiveIndex) {
-                return i;
-            }
+
+            if (i != removedPointerIndex && i != otherActiveIndex) { return i; }
+
         }
+
         return -1;
+
     }
 
     private void setContext(View view, MotionEvent curr) {
-        if (mCurrEvent != null) {
-            mCurrEvent.recycle();
-        }
-        mCurrEvent = MotionEvent.obtain(curr);
 
+        if (mCurrEvent != null) { mCurrEvent.recycle(); }
+        mCurrEvent = MotionEvent.obtain(curr);
         mCurrLen = -1;
         mPrevLen = -1;
         mScaleFactor = -1;
         mCurrSpanVector.set(0.0f, 0.0f);
-
         final MotionEvent prev = mPrevEvent;
-
         final int prevIndex0 = prev.findPointerIndex(mActiveId0);
         final int prevIndex1 = prev.findPointerIndex(mActiveId1);
         final int currIndex0 = curr.findPointerIndex(mActiveId0);
         final int currIndex1 = curr.findPointerIndex(mActiveId1);
 
         if (prevIndex0 < 0 || prevIndex1 < 0 || currIndex0 < 0 || currIndex1 < 0) {
+
             mInvalidGesture = true;
             Log.e(TAG, "Invalid MotionEvent stream detected.", new Throwable());
+
             if (mGestureInProgress) {
-                mListener.onScaleEnd(view, this);
-            }
+
+                mListener.onScaleEnd(view, this); }
             return;
         }
 
