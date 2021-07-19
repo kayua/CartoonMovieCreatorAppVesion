b6ce11d8592a90320a17d90dbbd1b86ffca6f3c9
kayua <kayuaolequesp@gmail.com>
2021-07-18 22:18:51 -0300

    version 1.0.1.1

--- src/main/java/app/mosquito/appmosquito/appmosquito/Editor/Shapes/TextShadow.java
+++ src/main/java/app/mosquito/appmosquito/appmosquito/Editor/Shapes/TextShadow.java
@@ -1,47 +1,35 @@
 package app.mosquito.appmosquito.appmosquito.Editor.Shapes;
 
 public class TextShadow {
+
     float radius;
     float dx;
     float dy;
     int color;
 
     public TextShadow(float radius, float dx, float dy, int color) {
+
         this.radius = radius;
         this.dx = dx;
         this.dy = dy;
         this.color = color;
+
     }
 
-    public float getRadius() {
-        return radius;
-    }
+    public float getRadius() { return radius; }
 
-    public void setRadius(float radius) {
-        this.radius = radius;
-    }
+    public void setRadius(float radius) { this.radius = radius; }
 
-    public float getDx() {
-        return dx;
-    }
+    public float getDx() { return dx; }
 
-    public void setDx(float dx) {
-        this.dx = dx;
-    }
+    public void setDx(float dx) { this.dx = dx; }
 
-    public float getDy() {
-        return dy;
-    }
+    public float getDy() { return dy; }
 
-    public void setDy(float dy) {
-        this.dy = dy;
-    }
+    public void setDy(float dy) { this.dy = dy; }
 
-    public int getColor() {
-        return color;
-    }
+    public int getColor() { return color; }
 
-    public void setColor(int color) {
-        this.color = color;
-    }
+    public void setColor(int color) { this.color = color; }
+
 }
