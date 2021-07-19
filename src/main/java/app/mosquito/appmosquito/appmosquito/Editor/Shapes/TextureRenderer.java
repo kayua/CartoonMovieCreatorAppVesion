14f3e9bbadf82612f86ac3900dae6bce2799485e
kayua <kayuaolequesp@gmail.com>
2021-07-18 22:26:12 -0300

    version 1.0.1.1

--- src/main/java/app/mosquito/appmosquito/appmosquito/Editor/Shapes/TextureRenderer.java
+++ src/main/java/app/mosquito/appmosquito/appmosquito/Editor/Shapes/TextureRenderer.java
@@ -1,23 +1,6 @@
-/*
- * Copyright (C) 2014 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package app.mosquito.appmosquito.appmosquito.Editor.Shapes;
 
 import android.opengl.GLES20;
-
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
@@ -66,7 +49,6 @@ class TextureRenderer {
     private static final int FLOAT_SIZE_BYTES = 4;
 
     void init() {
-        // Create program
         mProgram = GLToolbox.createProgram(VERTEX_SHADER, FRAGMENT_SHADER);
 
         // Bind attributes and uniforms
