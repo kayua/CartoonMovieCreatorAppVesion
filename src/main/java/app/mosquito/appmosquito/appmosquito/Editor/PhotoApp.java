ac7d3a8869ce470dba52a34c54c6fcdcea87e96d
kayua <kayuaolequesp@gmail.com>
2021-07-18 22:51:47 -0300

    version 1.0.1.1

--- src/main/java/app/mosquito/appmosquito/appmosquito/Editor/PhotoApp.java
+++ src/main/java/app/mosquito/appmosquito/appmosquito/Editor/PhotoApp.java
@@ -5,6 +5,7 @@ import android.content.Context;
 
 
 public class PhotoApp extends Application {
+
     private static PhotoApp sPhotoApp;
     private static final String TAG = PhotoApp.class.getSimpleName();
 
@@ -15,11 +16,8 @@ public class PhotoApp extends Application {
 
     }
 
-    public static PhotoApp getPhotoApp() {
-        return sPhotoApp;
-    }
+    public static PhotoApp getPhotoApp() { return sPhotoApp; }
 
-    public Context getContext() {
-        return sPhotoApp.getContext();
-    }
+    public Context getContext() { return sPhotoApp.getContext(); }
+
 }
