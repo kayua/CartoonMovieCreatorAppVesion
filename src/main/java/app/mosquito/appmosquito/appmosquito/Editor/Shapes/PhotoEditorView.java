fde65957ba9cd6a1b7ffa72db20163cc2cc7f614
kayua <kayuaolequesp@gmail.com>
2021-07-18 21:34:08 -0300

    version 1.0.1.1

--- src/main/java/app/mosquito/appmosquito/appmosquito/Editor/Shapes/PhotoEditorView.java
+++ src/main/java/app/mosquito/appmosquito/appmosquito/Editor/Shapes/PhotoEditorView.java
@@ -11,7 +11,6 @@ import android.util.Log;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
@@ -28,41 +27,51 @@ public class PhotoEditorView extends RelativeLayout {
     private static final int imgSrcId = 1, shapeSrcId = 2, glFilterId = 3;
 
     public PhotoEditorView(Context context) {
+
         super(context);
         init(null);
+
     }
 
     public PhotoEditorView(Context context, AttributeSet attrs) {
+
         super(context, attrs);
         init(attrs);
+
     }
 
     public PhotoEditorView(Context context, AttributeSet attrs, int defStyleAttr) {
+
         super(context, attrs, defStyleAttr);
         init(attrs);
+
     }
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public PhotoEditorView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+
         super(context, attrs, defStyleAttr, defStyleRes);
         init(attrs);
+
     }
 
     @SuppressLint("Recycle")
     private void init(@Nullable AttributeSet attrs) {
-        //Setup image attributes
+
         mImgSource = new FilterImageView(getContext());
         mImgSource.setId(imgSrcId);
         mImgSource.setAdjustViewBounds(true);
         LayoutParams imgSrcParam = new LayoutParams(
                 ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         imgSrcParam.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+
         if (attrs != null) {
+
             TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.PhotoEditorView);
             Drawable imgSrcDrawable = a.getDrawable(R.styleable.PhotoEditorView_photo_src);
-            if (imgSrcDrawable != null) {
-                mImgSource.setImageDrawable(imgSrcDrawable);
-            }
+
+            if (imgSrcDrawable != null) { mImgSource.setImageDrawable(imgSrcDrawable); }
+
         }
 
         mDrawingView = new DrawingView(getContext());
@@ -81,6 +90,7 @@ public class PhotoEditorView extends RelativeLayout {
 
         LayoutParams imgFilterParam = new LayoutParams(
                 ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
         imgFilterParam.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
         imgFilterParam.addRule(RelativeLayout.ALIGN_TOP, imgSrcId);
         imgFilterParam.addRule(RelativeLayout.ALIGN_BOTTOM, imgSrcId);
@@ -96,52 +106,62 @@ public class PhotoEditorView extends RelativeLayout {
 
 
         addView(mImgSource, imgSrcParam);
-
         addView(mImageFilterView, imgFilterParam);
-
         addView(mDrawingView, brushParam);
+
     }
 
 
-    public ImageView getSource() {
-        return mImgSource;
-    }
+    public ImageView getSource() { return mImgSource; }
 
-    DrawingView getDrawingView() {
-        return mDrawingView;
-    }
-
+    DrawingView getDrawingView() { return mDrawingView; }
 
     void saveFilter(@NonNull final OnSaveBitmap onSaveBitmap) {
+
         if (mImageFilterView.getVisibility() == VISIBLE) {
+
             mImageFilterView.saveBitmap(new OnSaveBitmap() {
+
                 @Override
                 public void onBitmapReady(final Bitmap saveBitmap) {
+
                     Log.e(TAG, "saveFilter: " + saveBitmap);
                     mImgSource.setImageBitmap(saveBitmap);
                     mImageFilterView.setVisibility(GONE);
                     onSaveBitmap.onBitmapReady(saveBitmap);
+
                 }
 
                 @Override
+
                 public void onFailure(Exception e) {
                     onSaveBitmap.onFailure(e);
                 }
+
             });
+
         } else {
+
             onSaveBitmap.onBitmapReady(mImgSource.getBitmap());
+
         }
+
     }
 
     void setFilterEffect(PhotoFilter filterType) {
+
         mImageFilterView.setVisibility(VISIBLE);
         mImageFilterView.setSourceBitmap(mImgSource.getBitmap());
         mImageFilterView.setFilterEffect(filterType);
+
     }
 
     void setFilterEffect(CustomEffect customEffect) {
+
         mImageFilterView.setVisibility(VISIBLE);
         mImageFilterView.setSourceBitmap(mImgSource.getBitmap());
         mImageFilterView.setFilterEffect(customEffect);
+
     }
+
 }
