0dd371d13bff1b2335dab41b477b6e2750428eb2
kayua <kayuaolequesp@gmail.com>
2021-07-18 21:01:32 -0300

    version 1.0.1.1

--- src/main/java/app/mosquito/appmosquito/appmosquito/Camera/CameraPreview.java
+++ src/main/java/app/mosquito/appmosquito/appmosquito/Camera/CameraPreview.java
@@ -5,7 +5,6 @@ import android.hardware.Camera;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-
 import java.io.IOException;
 
 import static android.content.ContentValues.TAG;
@@ -18,7 +17,6 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
     public CameraPreview(Context context, Camera camera) {
         super(context);
         mCamera = camera;
-
         mHolder = getHolder();
         mHolder.addCallback(this);
         mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
@@ -27,10 +25,14 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
     public void surfaceCreated(SurfaceHolder holder) {
 
         try {
+
             mCamera.setPreviewDisplay(holder);
             mCamera.startPreview();
+
         } catch (IOException e) {
+
             Log.d(TAG, "Error setting camera preview: " + e.getMessage());
+
         }
     }
 
@@ -47,13 +49,16 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
         }
 
         try {
+
             mCamera.stopPreview();
+
         } catch (Exception e){
 
         }
 
 
         try {
+
             mCamera.setPreviewDisplay(mHolder);
             mCamera.startPreview();
 
