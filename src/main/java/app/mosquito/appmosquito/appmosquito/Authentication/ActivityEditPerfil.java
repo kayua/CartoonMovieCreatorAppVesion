9f910b3852457953cc3832a3720d719d148ee047
kayua <kayuaolequesp@gmail.com>
2021-07-24 19:16:37 -0300

    version 1.0.1.1

--- src/main/java/app/mosquito/appmosquito/appmosquito/Authentication/ActivityEditPerfil.java
+++ src/main/java/app/mosquito/appmosquito/appmosquito/Authentication/ActivityEditPerfil.java
@@ -10,7 +10,7 @@ import app.mosquito.appmosquito.appmosquito.ActivityUserInterface;
 import app.mosquito.appmosquito.appmosquito.R;
 
 
-public class ActivityPerfil extends Activity {
+public class ActivityEditPerfil extends Activity {
 
 
     @Override
@@ -31,7 +31,7 @@ public class ActivityPerfil extends Activity {
 
     }
     private void screen_user() {
-        Intent i = new Intent(ActivityPerfil.this, ActivityUserInterface.class);
+        Intent i = new Intent(ActivityEditPerfil.this, ActivityUserInterface.class);
         finish();
         startActivity(i);
     }
