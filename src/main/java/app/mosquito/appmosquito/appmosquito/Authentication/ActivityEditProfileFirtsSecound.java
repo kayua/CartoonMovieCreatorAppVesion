0eba470ab1974800e626ee8cb0fb09f767a7a289
kayua <kayuaolequesp@gmail.com>
2021-07-24 19:21:03 -0300

    version 1.0.1.1

---
+++ src/main/java/app/mosquito/appmosquito/appmosquito/Authentication/ActivityEditProfileFirtsSecound.java
@@ -0,0 +1,50 @@
+package app.mosquito.appmosquito.appmosquito.Authentication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import app.mosquito.appmosquito.appmosquito.ActivityUserInterface;
+import app.mosquito.appmosquito.appmosquito.R;
+
+
+public class ActivityEditProfileFirtsSecound extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.auth_perfil);
+
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+    }
+    private void acess(String textBoxUsername, String textBoxPassword){
+
+
+
+    }
+    private void screen_user() {
+        Intent i = new Intent(ActivityEditProfileFirtsSecound.this, ActivityUserInterface.class);
+        finish();
+        startActivity(i);
+    }
+
+    private void toast() {
+        Toast.makeText(getApplicationContext(), "Não foi possível encontrar sua conta. Verificar se o preenchimento está correto.", Toast.LENGTH_SHORT).show();
+            }
+    @Override
+
+    public void onStart() {
+        super.onStart();
+
+    }
+}
+
+
