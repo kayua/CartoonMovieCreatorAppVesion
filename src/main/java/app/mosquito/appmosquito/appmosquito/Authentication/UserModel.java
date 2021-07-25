02ae74d66fe5682aba29ccc07338040c564e812d
kayua <kayuaolequesp@gmail.com>
2021-07-24 22:25:29 -0300

    version 1.0.1.1

--- src/main/java/app/mosquito/appmosquito/appmosquito/Authentication/UserModel.java
+++ src/main/java/app/mosquito/appmosquito/appmosquito/Authentication/UserModel.java
@@ -1,5 +1,8 @@
 package app.mosquito.appmosquito.appmosquito.Authentication;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class UserModel {
 
     String idUser;
@@ -21,6 +24,45 @@ public class UserModel {
         this.userSchooling = userSchooling;
         this.userFavoriteWord = userFavoriteWord;
         this.userGender = userGender;
+    }
+
+    public String getIdUser() { return idUser; }
+
+    public void setIdUser(String idUser) { this.idUser = idUser; }
+
+    public String getUserFirstName() { return userFirstName; }
+
+    public void setUserFirstName(String userFirstName) { this.userFirstName = userFirstName; }
+
+    public String getUserSecondName() { return userSecondName; }
+
+    public void setUserSecondName(String userSecondName) { this.userSecondName = userSecondName; }
+
+    public String getUserCity() { return userCity; }
+
+    public void setUserCity(String userCity) { this.userCity = userCity; }
+
+    public String getUserBirthDate() { return userBirthDate; }
+
+    public void setUserBirthDate(String userBirthDate) { this.userBirthDate = userBirthDate; }
+
+    public String getUserSchooling() { return userSchooling; }
+
+    public void setUserSchooling(String userSchooling) { this.userSchooling = userSchooling; }
+
+    public String getUserFavoriteWord() { return userFavoriteWord; }
+
+    public void setUserFavoriteWord(String userFavoriteWord) {
+        this.userFavoriteWord = userFavoriteWord; }
+
+    public String getUserGender() { return userGender; }
+
+    public void setUserGender(String userGender) { this.userGender = userGender; }
+
+    public void saveDataFirebase(){
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+        reference.child("user_info").child(getIdUser()).setValue(this);
     }
 
 }
