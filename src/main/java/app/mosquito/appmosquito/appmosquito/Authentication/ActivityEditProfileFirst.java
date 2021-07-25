69e3b3fc3d3e3511c742d17d7d0cfe11dd88234f
kayua <kayuaolequesp@gmail.com>
2021-07-24 22:41:32 -0300

    version 1.0.1.1

--- src/main/java/app/mosquito/appmosquito/appmosquito/Authentication/ActivityEditProfileFirst.java
+++ src/main/java/app/mosquito/appmosquito/appmosquito/Authentication/ActivityEditProfileFirst.java
@@ -2,8 +2,11 @@ package app.mosquito.appmosquito.appmosquito.Authentication;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.Toast;
 
 import app.mosquito.appmosquito.appmosquito.ActivityUserInterface;
@@ -12,6 +15,7 @@ import app.mosquito.appmosquito.appmosquito.R;
 
 public class ActivityEditProfileFirst extends Activity {
 
+    public static final String PREFS_NAME = "PersonalDatabase";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,20 +24,38 @@ public class ActivityEditProfileFirst extends Activity {
 
         setContentView(R.layout.auth_profile_first);
 
+        Button next_step =  findViewById(R.id.buttonAuthAcessRegisterAccount);
+
+        next_step.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+            }
+
+        });
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-
     }
-    private void acess(String textBoxUsername, String textBoxPassword){
 
-
-
-    }
     private void screen_user() {
+
         Intent i = new Intent(ActivityEditProfileFirst.this, ActivityUserInterface.class);
         finish();
         startActivity(i);
+
+    }
+
+    private void store_login(String birthDate, String city, String company) {
+
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString("birthDate", birthDate);
+        editor.putString("city",city);
+        editor.putString("company", company);
+        editor.apply();
+
     }
 
     private void toast() {
