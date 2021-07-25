dc1e00a5dc73d9b75c2094a6f3ea766dbb979fad
kayua <kayuaolequesp@gmail.com>
2021-07-24 22:13:22 -0300

    version 1.0.1.1

--- src/main/java/app/mosquito/appmosquito/appmosquito/Authentication/UserModel.java
+++ src/main/java/app/mosquito/appmosquito/appmosquito/Authentication/UserModel.java
@@ -1,4 +1,26 @@
 package app.mosquito.appmosquito.appmosquito.Authentication;
 
 public class UserModel {
+
+    String idUser;
+    String userFirstName;
+    String userSecondName;
+    String userCity;
+    String userBirthDate;
+    String userSchooling;
+    String userFavoriteWord;
+    String userGender;
+
+    public UserModel(String idUser, String userFirstName, String userSecondName, String userCity, String userBirthDate, String userSchooling, String userFavoriteWord, String userGender) {
+
+        this.idUser = idUser;
+        this.userFirstName = userFirstName;
+        this.userSecondName = userSecondName;
+        this.userCity = userCity;
+        this.userBirthDate = userBirthDate;
+        this.userSchooling = userSchooling;
+        this.userFavoriteWord = userFavoriteWord;
+        this.userGender = userGender;
+    }
+
 }
