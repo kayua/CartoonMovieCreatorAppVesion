cc8c539e35e29578eec74050d262f51574e3f920
kayua <kayuaolequesp@gmail.com>
2021-07-25 21:33:07 -0300

    version 1.0.1.1

---
+++ src/main/java/app/mosquito/appmosquito/appmosquito/Editor/AppController.java
@@ -0,0 +1,20 @@
+package app.mosquito.appmosquito.appmosquito.Editor;
+
+import android.app.Application;
+
+@SuppressWarnings("unused") public class AppController extends Application {
+  private static final String TAG = com.example.simplecropviewsample.AppController.class.getSimpleName();
+  private static com.example.simplecropviewsample.AppController instance;
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    instance = this;
+    // load custom font
+    com.example.simplecropviewsample.FontUtils.loadFont(getApplicationContext(), "Roboto-Light.ttf");
+  }
+
+  public static com.example.simplecropviewsample.AppController getInstance() {
+    return instance;
+  }
+}
