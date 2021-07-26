b0dac1fd8adbdd4fc078e105442e12a1d67e2001
kayua <kayuaolequesp@gmail.com>
2021-07-26 00:03:36 -0300

    version 1.0.1.1

--- src/main/java/app/mosquito/appmosquito/appmosquito/Authentication/ActivityUserImage.java
+++ src/main/java/app/mosquito/appmosquito/appmosquito/Authentication/ActivityUserImage.java
@@ -1,10 +1,12 @@
 package app.mosquito.appmosquito.appmosquito.Authentication;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -16,6 +18,7 @@ import app.mosquito.appmosquito.appmosquito.R;
 public class ActivityUserImage extends Activity {
 
 
+    @SuppressLint("WrongThread")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -27,11 +30,30 @@ public class ActivityUserImage extends Activity {
 
         ImageView imageView1;
         RoundImage roundedImage;
+        int PICK_IMAGE = 1;
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
 
         imageView1 = (ImageView) findViewById(R.id.imageView23);
         Bitmap bm = BitmapFactory.decodeResource(getResources(),R.drawable.example2);
         roundedImage = new RoundImage(bm);
         imageView1.setImageDrawable(roundedImage);
+
+        ImageView buttonLoadImage = (ImageView) findViewById(R.id.imageView23);
+        buttonLoadImage.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent i = new Intent(
+                        Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
+            }
+        });
 
 
     }
@@ -40,6 +62,34 @@ public class ActivityUserImage extends Activity {
         Intent i = new Intent(ActivityUserImage.this, ActivityUserInterface.class);
         finish();
         startActivity(i);
+    }
+    private static int RESULT_LOAD_IMAGE = 1;
+
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            ImageView imageView = (ImageView) findViewById(R.id.imgView);
+            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+
+        }
+
+
     }
 
     private void toast() {
