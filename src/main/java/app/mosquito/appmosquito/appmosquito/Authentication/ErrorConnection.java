e12668a8eef30ad8e4380686d380ff801145e426
kayua <kayuaolequesp@gmail.com>
2021-07-28 14:06:07 -0300

    version 1.0.1.1

--- src/main/java/app/mosquito/appmosquito/appmosquito/Authentication/ErrorConnection.java
+++ src/main/java/app/mosquito/appmosquito/appmosquito/Authentication/ErrorConnection.java
@@ -3,9 +3,11 @@ package app.mosquito.appmosquito.appmosquito.Authentication;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
 
-import app.mosquito.appmosquito.appmosquito.MainUserInterface;
 import app.mosquito.appmosquito.appmosquito.R;
 
 
@@ -19,13 +21,38 @@ public class ErrorConnection extends Activity {
         setContentView(R.layout.auth_not_connected);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        Button buttonRetry = (Button) findViewById(R.id.buttonAuthAcessRegisterAccount3);
+        TextView textVieCloseApp = (Button) findViewById(R.id.buttonAuthAcessRegisterAccount3);
+
+
+
+        buttonRetry.setOnClickListener( new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) { screenStartUser(); }});
+
+        textVieCloseApp.setOnClickListener( new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) { closeApplication(); }});
+
+
     }
 
     private void screenStartUser() {
+
         Intent i = new Intent(ErrorConnection.this, MenuSource.class);
         finish();
         startActivity(i);
+
     }
+    private void closeApplication(){
+
+        this.finishAffinity();
+
+    }
+
+
 
     @Override
 
@@ -33,6 +60,7 @@ public class ErrorConnection extends Activity {
         super.onStart();
 
     }
+
 }
 
 
