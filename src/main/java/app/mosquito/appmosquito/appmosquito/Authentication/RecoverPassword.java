00573c58eada9f3ade6437333455a560768b29bf
kayua <kayuaolequesp@gmail.com>
2021-07-28 14:14:18 -0300

    version 1.0.1.1

--- src/main/java/app/mosquito/appmosquito/appmosquito/Authentication/RecoverPassword.java
+++ src/main/java/app/mosquito/appmosquito/appmosquito/Authentication/RecoverPassword.java
@@ -4,21 +4,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.Switch;
 import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.auth.FirebaseAuth;
+
 import app.mosquito.appmosquito.appmosquito.R;
 
 
 public class RecoverPassword extends AppCompatActivity {
 
     public static final String PREFS_NAME = "PersonalDatabase";
-    private FirebaseAuth.AuthStateListener mAuthListener;
-    private Switch active_email_notification, active_realtime_notification, active_infestations_detect;
-
-    public RecoverPassword() {
-    }
+    private FirebaseAuth.AuthStateListener AuthenticationListener;
 
 
     @Override
@@ -26,15 +24,16 @@ public class RecoverPassword extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.auth_recovery);
-        TextView goBack= (TextView) findViewById(R.id.textView18);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        setContentView(R.layout.auth_recovery);
+        TextView textViewMenuSource= (TextView) findViewById(R.id.textView18);
 
-        goBack.setOnClickListener(new View.OnClickListener() {
+        textViewMenuSource.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
+
                 Intent i = new Intent(getApplicationContext(), MenuSource.class);
                 finish();
                 startActivity(i);
@@ -43,20 +42,20 @@ public class RecoverPassword extends AppCompatActivity {
         });
 
     }
+
     @Override
     public void onStart() {
+
         super.onStart();
 
     }
 
     @Override
     public void onStop() {
+
         super.onStop();
-    }
-
-
-    public void createUser(View view) throws InterruptedException {
-
 
     }
+
+
 }
