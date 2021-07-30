d17119ed7204d3d5b21046ab05cbe25bd610d93f
kayua <kayuaolequesp@gmail.com>
2021-07-30 15:34:19 -0300

    version 1.0.1.1

---
+++ src/main/java/app/mosquito/appmosquito/appmosquito/Posts/FollowerFragment.java
@@ -0,0 +1,104 @@
+package app.mosquito.appmosquito.appmosquito.Posts;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import app.mosquito.appmosquito.appmosquito.R;
+
+public class FollowerFragment extends Fragment {
+
+    private MessageViewModel galleryViewModel;
+
+    ArrayAdapter<String> adapter;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        galleryViewModel = new ViewModelProvider(this).get(MessageViewModel.class);
+
+        View root = inflater.inflate(R.layout.fragment_message, container, false);
+        ListView list = root.findViewById(R.id.chat_list_view);
+
+        ListView lista = (ListView) root.findViewById(R.id.chat_list_view);
+        List<PostStructure> cursos = todosOsCursos();
+        ViewAdapter adapter = new ViewAdapter(cursos, getActivity());
+        lista.setAdapter(adapter);
+
+
+        galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+
+            }
+        });
+        return root;
+    }
+    private List<PostStructure> todosOsCursos() {
+
+        PostStructure postOne = new PostStructure();
+        PostStructure postTwo = new PostStructure();
+        PostStructure postThree = new PostStructure();
+        PostStructure postFour = new PostStructure();
+        PostStructure postHeader = new PostStructure();
+
+        postOne.setPostName("Primeiro Post");
+        postOne.setPostUserName("Kayuã Oleques Paim");
+        postOne.setPostDate("28/07/21 23:36");
+        postOne.setPostCategorical(CategoricalPosts.postSuggest);
+        postOne.setPostLocalization("Alegrete - RS");
+        postOne.setPostNumberLikes(100);
+        postOne.setPostNumberShares(31);
+        postOne.setPostNumberComments(31);
+        postOne.setPostText("Texto de exemplo post");
+        postOne.setPostState(EstadoAtual.FAZENDO);
+
+
+        postThree.setPostName("Terceiro Post");
+        postThree.setPostUserName("Kayuã Oleques Paim");
+        postThree.setPostDate("28/07/21 23:36");
+        postThree.setPostText("Texto de exemplo post");
+        postThree.setPostCategorical(CategoricalPosts.postMovie);
+        postThree.setPostLocalization("Alegrete - RS");
+        postThree.setPostNumberLikes(100);
+        postThree.setPostNumberShares(31);
+        postThree.setPostNumberComments(31);
+        postThree.setPostState(EstadoAtual.FAZENDO);
+
+        postFour.setPostName("Terceiro Post");
+        postFour.setPostUserName("Kayuã Oleques Paim");
+        postFour.setPostDate("28/07/21 23:36");
+        postFour.setPostText("Texto de exemplo post");
+        postFour.setPostCategorical(CategoricalPosts.postText);
+        postFour.setPostLocalization("Alegrete - RS");
+        postFour.setPostNumberLikes(100);
+        postFour.setPostNumberShares(31);
+        postFour.setPostNumberComments(31);
+        postFour.setPostState(EstadoAtual.FAZENDO);
+
+        postHeader.setPostName("Terceiro Post");
+        postHeader.setPostUserName("Kayuã Oleques Paim");
+        postHeader.setPostDate("28/07/21 23:36");
+        postHeader.setPostText("Texto de exemplo post");
+        postHeader.setPostCategorical(CategoricalPosts.postText);
+        postHeader.setPostLocalization("Alegrete - RS");
+        postHeader.setPostNumberLikes(100);
+        postHeader.setPostNumberShares(31);
+        postHeader.setPostNumberComments(31);
+        postHeader.setPostState(EstadoAtual.FAZENDO);
+
+    return new ArrayList<>(Arrays.asList(postHeader, postOne, postThree, postFour));
+}
+}
---
+++ src/main/java/app/mosquito/appmosquito/appmosquito/Posts/FollowerStructure.java
@@ -0,0 +1,46 @@
+package app.mosquito.appmosquito.appmosquito.Posts;
+
+public class FollowerStructure {
+
+    private String postName;
+    private String postUserName;
+    private String postDate;
+    private String postText;
+    private String postLocalization;
+    private int postNumberLikes;
+    private int postNumberShares;
+    private int postNumberComments;
+    private EstadoAtual postState;
+    private CategoricalPosts postCategorical;
+
+    public FollowerStructure() { }
+
+    public String getPostName() { return postName; }
+    public String getPostUserName() { return postUserName; }
+    public String getPostDate() { return postDate; }
+    public String getPostText() { return postText; }
+    public String getPostLocalization() { return postLocalization; }
+    public int getPostNumberLikes() { return postNumberLikes; }
+    public int getPostNumberShares() { return postNumberShares; }
+    public int getPostNumberComments() { return postNumberComments; }
+    public EstadoAtual getPostState() { return postState; }
+    public CategoricalPosts getPostCategorical() { return postCategorical; }
+
+    public void setPostUserName(String postUserName) { this.postUserName = postUserName; }
+    public void setPostName(String postName) { this.postName = postName; }
+    public void setPostDate(String postDate) { this.postDate = postDate; }
+    public void setPostText(String postText) { this.postText = postText; }
+    public void setPostLocalization(String postLocalization) { this.postLocalization = postLocalization; }
+    public void setPostNumberLikes(int postNumberLikes) { this.postNumberLikes = postNumberLikes; }
+    public void setPostNumberShares(int postNumberShares) { this.postNumberShares = postNumberShares; }
+    public void setPostNumberComments(int postNumberComments) { this.postNumberComments = postNumberComments; }
+    public void setPostState(EstadoAtual postState) { this.postState = postState; }
+    public void setPostCategorical(CategoricalPosts postCategorical) { this.postCategorical = postCategorical; }
+
+    @Override
+    public String toString() {
+
+        return "Curso: " + postName + " Descrição: " +
+                postNumberLikes + " Estado: " + postState;
+    }
+}
