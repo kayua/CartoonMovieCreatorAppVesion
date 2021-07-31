6681a182f7d0c34c66cbed54b20d077db9f5d469
kayua <kayuaolequesp@gmail.com>
2021-07-31 06:48:28 -0300

    version 1.0.1.1

--- src/main/java/app/mosquito/appmosquito/appmosquito/Editor/PropertiesBSFragment.java
+++ src/main/java/app/mosquito/appmosquito/appmosquito/Editor/PropertiesBSFragment.java
@@ -33,7 +33,7 @@ public class PropertiesBSFragment extends BottomSheetDialogFragment implements S
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_bottom_properties_dialog, container, false);
+        return inflater.inflate(R.layout.layout_editor_bottom_properties, container, false);
 
     }
 
