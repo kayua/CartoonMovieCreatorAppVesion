5dbf1df2e92c120f2cd903914b144c734c929d39
kayua <kayuaolequesp@gmail.com>
2021-07-31 21:18:18 -0300

    version 1.0.1.1

--- src/main/java/app/mosquito/appmosquito/appmosquito/Camera/CameraActivity.java
+++ src/main/java/app/mosquito/appmosquito/appmosquito/Camera/CameraActivity.java
@@ -62,6 +62,13 @@ public class CameraActivity extends AppCompatActivity {
         FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
         preview.addView(mPreview);
         ImageView captureButton = findViewById(R.id.button_record);
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 
 
         captureButton.setOnClickListener(
