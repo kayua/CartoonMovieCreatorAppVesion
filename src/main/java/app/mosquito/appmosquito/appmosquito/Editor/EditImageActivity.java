4ae721e6edfa63983e808f0bd4f37105cc5210b1
kayua <kayuaolequesp@gmail.com>
2021-08-03 18:07:07 -0300

    version 1.0.1.1

--- src/main/java/app/mosquito/appmosquito/appmosquito/Editor/EditImageActivity.java
+++ src/main/java/app/mosquito/appmosquito/appmosquito/Editor/EditImageActivity.java
@@ -454,6 +454,18 @@ public class EditImageActivity extends BaseActivity implements OnPhotoEditorList
 
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        this.getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
+    }
 
     @Override
 
