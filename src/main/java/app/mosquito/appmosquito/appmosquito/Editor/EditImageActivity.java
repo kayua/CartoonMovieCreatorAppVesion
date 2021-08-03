bbe5f2d0169bc9265edb60d0a9f61751ddb4ad44
kayua <kayuaolequesp@gmail.com>
2021-08-03 17:24:15 -0300

    version 1.0.1.1

--- src/main/java/app/mosquito/appmosquito/appmosquito/Editor/EditImageActivity.java
+++ src/main/java/app/mosquito/appmosquito/appmosquito/Editor/EditImageActivity.java
@@ -100,14 +100,21 @@ public class EditImageActivity extends BaseActivity implements OnPhotoEditorList
 
         super.onCreate(savedInstanceState);
         makeFullScreen();
-        getWindow().setNavigationBarColor(ContextCompat.getColor(this, R.color.sky_blue_color_picker));
 
         setContentView(R.layout.layout_image_editor_main_view);
         conteudo = findViewById(R.id.imgShare);
         initViews();
         myButton = findViewById(R.id.imgShare);
         myView = findViewById(R.id.myd_view);
+        this.getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 
+        onSlideViewButtonClick(myButton);
         myButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -280,6 +287,7 @@ public class EditImageActivity extends BaseActivity implements OnPhotoEditorList
     @Override
     public void onClick(View view) {
 
+
         switch (view.getId()) {
 
             case R.id.imgUndo:
@@ -349,7 +357,6 @@ public class EditImageActivity extends BaseActivity implements OnPhotoEditorList
 
     }
 
-
     private void saveImage() {
 
         final String fileName = System.currentTimeMillis() + ".png";
@@ -410,7 +417,6 @@ public class EditImageActivity extends BaseActivity implements OnPhotoEditorList
 
     }
 
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
@@ -449,7 +455,6 @@ public class EditImageActivity extends BaseActivity implements OnPhotoEditorList
 
 
     @Override
-
     public void onColorChanged(int colorCode) {
 
         mPhotoEditor.setShape(mShapeBuilder.withShapeColor(colorCode));
@@ -583,7 +588,6 @@ public class EditImageActivity extends BaseActivity implements OnPhotoEditorList
 
     }
 
-
     void showFilter(boolean isVisible) {
 
         mIsFilterVisible = isVisible;
@@ -630,7 +634,6 @@ public class EditImageActivity extends BaseActivity implements OnPhotoEditorList
         }
 
     }
-
 
     public void slideUp(View view){
 
